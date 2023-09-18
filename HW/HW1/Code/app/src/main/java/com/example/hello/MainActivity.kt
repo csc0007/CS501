64d1dbb1ade3e7380b40fa0b4882b405ec1a7502
Sicheng Chen <csc0007@outlook.com>
2023-09-18 17:33:23 -0400

    work done

--- HW/HW1/Code/app/src/main/java/com/example/hello/MainActivity.kt
+++ HW/HW1/Code/app/src/main/java/com/example/hello/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.hello
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    val myName = "Sicheng Chen"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.textView)  //bind the textView
+        textView.text = "Hi, my name is $myName."   //Used variable myName to display content Sicheng Chen
     }
 }
